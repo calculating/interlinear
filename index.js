35f1ea5a99579aa9093f0b2a9ce5c899c634adfc
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-12-02 11:28:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function crack() {
     document.getElementsByTagName('body')[0].innerHTML = "";
 
     function wordSearch(word, def = 0) {
-        
+
         console.log(word);
         var url = "https://en.wiktionary.org/w/api.php?action=parse&page=" + word + "&format=json&origin=*";
 
@@ -16,7 +16,7 @@ function crack() {
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4) {
                 raw = xhr.responseText;
-                jason=JSON.parse(xhr.responseText);
+                jason = JSON.parse(xhr.responseText);
                 res = "oop";
                 if (typeof jason['parse'] !== "undefined") {
                     if (typeof jason['parse']['text'] !== "undefined") {
@@ -30,38 +30,38 @@ function crack() {
                     res = res.split('/')[2].split('#Latin')[0];
                     console.log('next word is ' + res);
                     wordSearch(res, 1);
-                } else if (res.includes('<h2><span class="mw-headline" id="Latin">')){
+                } else if (res.includes('<h2><span class="mw-headline" id="Latin">')) {
                     res = res.split('<h2><span class="mw-headline" id="Latin">', 2)[1];
                     res = res.substring(res.split(/(noun<\/span><span class="mw-editsection">|pronoun<\/span><span class="mw-editsection">|verb<\/span><span class="mw-editsection">|adjective<\/span><span class="mw-editsection">|adverb<\/span><span class="mw-editsection">|preposition<\/span><span class="mw-editsection">|conjunction<\/span><span class="mw-editsection">|interjection<\/span><span class="mw-editsection">)/i)[0].lastIndexOf("<h"), res.length);
                     res = res.substring(0, res.search('</ol>'));
-                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:20%; font-size:15px; float:left;"><h2>'+word+'</h2>'+res+'</div>';
+                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:20%; font-size:15px; float:left;"><h2>' + word + '</h2>' + res + '</div>';
                 } else {
-                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:20%; font-size:15px; float:left;"><h2>'+word+'</h2>undefined</div>';
+                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:20%; font-size:15px; float:left;"><h2>' + word + '</h2>undefined</div>';
+                }
+                document.querySelectorAll("dl").forEach(e => e.remove());
+                document.querySelectorAll("ul").forEach(e => e.remove());
+                document.querySelectorAll(".thumbinner").forEach(e => e.remove());
+                var anchors = document.querySelectorAll("A");
+                for (var i = 0; i < anchors.length; i++) {
+                    var span = document.createElement("SPAN");
+                    if (anchors[i].className) {
+                        span.className = anchors[i].className;
+                    }
+
+                    if (anchors[i].id) {
+                        span.id = anchors[i].id;
+                    }
+
+                    span.innerHTML = anchors[i].innerHTML;
+
+                    anchors[i].parentNode.replaceChild(span, anchors[i]);
                 }
             }
         }
 
         xhr.send();
     }
-    
-    paste.forEach(word => wordSearch(word));    
-    document.querySelectorAll("dl").forEach(e => e.remove());
-    document.querySelectorAll("ul").forEach(e => e.remove());
-    document.querySelectorAll(".thumbinner").forEach(e => e.remove());
-    var anchors = document.querySelectorAll("A");
-    for ( var i=0; i < anchors.length; i++ ) {
-       var span = document.createElement("SPAN");
-        if ( anchors[i].className ) {
-            span.className = anchors[i].className;
-        }
 
-        if ( anchors[i].id ) {
-            span.id = anchors[i].id;
-        }
-
-        span.innerHTML = anchors[i].innerHTML;
-
-        anchors[i].parentNode.replaceChild(span, anchors[i]);
-    }
+    paste.forEach(word => wordSearch(word));
 
 }
