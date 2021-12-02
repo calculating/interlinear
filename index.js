a4fbd8bca7f09ac86524d390c79f6a0b7b004a65
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-12-02 11:31:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function crack() {
     paste = document.getElementsByName('latinpaste')[0].value;
     paste = paste.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().split(/\W+/);
+    console.log(paste);
     document.getElementsByTagName('body')[0].innerHTML = "";
 
     function wordSearch(word, def = 0) {
 
-        console.log(word);
         var url = "https://en.wiktionary.org/w/api.php?action=parse&page=" + word + "&format=json&origin=*";
 
         var xhr = new XMLHttpRequest();
@@ -28,7 +28,7 @@ function crack() {
                 if (raw.includes('Latin_non-lemma_forms') && def == 0) {
                     res = res.substring(res.indexOf('form-of'), res.length).split('class="Latn mention" lang="la"')[1].split('href')[1].split('"', 3)[1];
                     res = res.split('/')[2].split('#Latin')[0];
-                    console.log('next word is ' + res);
+                    //console.log('next word is ' + res);
                     wordSearch(res, 1);
                 } else if (res.includes('<h2><span class="mw-headline" id="Latin">')) {
                     res = res.split('<h2><span class="mw-headline" id="Latin">', 2)[1];
@@ -62,6 +62,8 @@ function crack() {
         xhr.send();
     }
 
-    paste.forEach(word => wordSearch(word));
+    for (var i = 0; i < paste.length; i++) {
+        wordSearch(paste[i])
+    }
 
 }
