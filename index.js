5c3d355a6dbdd8af547d8ef9937849212d067b82
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-12-02 10:40:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 function crack() {
-    pData = document.getElementsByName('fpacket')[0].value;
-    pData = pData.split("</option>")
-    pData.splice(-1)
-    classArr = [];
-    pData.forEach(classify)
-    function classify(x, i) {
-        long = x.slice(x.lastIndexOf(">") + 1)
-        medium = long.slice(long.indexOf(" ")) + "<br>"
-        short = medium.slice(0, medium.lastIndexOf("("))
+    paste = document.getElementsByName('paste')[0].value;
+    paste = paste.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().split(/\W+/);
+    document.getElementsByTagName('body')[0].innerHTML = "";
 
-        if (short[short.length - 1] == "1") {
-            short += "st"
-        } else if (short[short.length - 1] == "2") {
-            short += "nd"
-        } else if (short[short.length - 1] == "3") {
-            short += "rd"
-        } else {
-            short += "th"
+    paste.forEach(word => wordSearch(word));    
+
+    function wordSearch(word, def = 0) {
+        
+        console.log(word);
+        var url = "https://en.wiktionary.org/w/api.php?action=parse&page=" + word + "&format=json&origin=*";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+
+        xhr.setRequestHeader("Accept", "*/*");
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                raw = xhr.responseText;
+                res = JSON.parse(xhr.responseText)['parse']['text']['*'];
+                if (raw.includes('Latin_non-lemma_forms') && def == 0) {
+                    res = res.substring(res.indexOf('form-of'), res.length).split('class="Latn mention" lang="la"')[1].split('href')[1].split('"', 3)[1];
+                    res = res.split('/')[2].split('#Latin')[0];
+                    console.log('next word is ' + res);
+                    wordSearch(res, 1);
+                } else {
+                    res = res.split('<h2><span class="mw-headline" id="Latin">', 2)[1];
+                    res = res.substring(res.split(/[noun|pronoun|verb|adjective|adverb|preposition|conjunction|interjection]<\/span><span class="mw-editsection">/i)[0].lastIndexOf("<h"), res.length);
+                    res = res.substring(0, res.search('</ol>'));
+                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:10%; font-size:15px"><h2>'+word+'</h2>'+res+'</div>';
+                    
+
+
+                }
+            }
         }
-        short += " period"
 
-        teach = medium.slice(medium.lastIndexOf(")")+3)
-
-        classArr[parseInt(short[short.length - 10])] = short + teach + "<br>"
+        xhr.send();
     }
+document.querySelectorAll("dl").forEach(e => e.remove());
+                    document.querySelectorAll("ul").forEach(e => e.remove());
+                    document.querySelectorAll(".thumbinner").forEach(e => e.remove());
 
-    classes = ""
-
-    classArr.forEach(e => classes += e);
-    document.getElementsByClassName("slideContainer")[0].remove()
-    document.getElementsByClassName("watermark")[0].innerHTML = "schedulecheck.org"
-    document.getElementById("result").innerHTML = classes
 }
-
