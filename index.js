00bc615c5aff8ec82cc4108215722892db5987a0
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-12-02 11:50:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 function crack() {
     paste = document.getElementsByName('latinpaste')[0].value;
     paste = paste.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().split(/\W+/);
@@ -5,7 +8,6 @@ function crack() {
     document.getElementsByTagName('body')[0].innerHTML = "";
 
     function wordSearch(word, def = 0) {
-        document.getElementsByTagName('body')[0].innerHTML += '<div style="width:20%; font-size:15px; float:left;"><h2>' + word + '</h2></div>';
 
         var url = "https://en.wiktionary.org/w/api.php?action=parse&page=" + word + "&format=json&origin=*";
 
@@ -35,8 +37,10 @@ function crack() {
                     res = res.split('<h2><span class="mw-headline" id="Latin">', 2)[1];
                     res = res.substring(res.split(/(noun<\/span><span class="mw-editsection">|pronoun<\/span><span class="mw-editsection">|verb<\/span><span class="mw-editsection">|adjective<\/span><span class="mw-editsection">|adverb<\/span><span class="mw-editsection">|preposition<\/span><span class="mw-editsection">|conjunction<\/span><span class="mw-editsection">|interjection<\/span><span class="mw-editsection">)/i)[0].lastIndexOf("<h"), res.length);
                     res = res.substring(0, res.search('</ol>'));
-                    document.getElementsByTagName('div')[document.getElementsByTagName('div').length-1].innerHTML += res;
-                } 
+                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:20%; font-size:12px; float:left;"><h2>' + word + '</h2>'+res+'</div>';
+                } else {
+                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:20%; font-size:12px; float:left;"><h2>' + word + '</h2>undefined</div>';
+                }
                 document.querySelectorAll("dl").forEach(e => e.remove());
                 document.querySelectorAll("ul").forEach(e => e.remove());
                 document.querySelectorAll(".thumbinner").forEach(e => e.remove());
@@ -62,7 +66,8 @@ function crack() {
     }
 
     for (var i = 0; i < paste.length; i++) {
-        wordSearch(paste[i])
+        wordSearch(paste[i]);
+        await sleep(1000);
     }
 
 }
