3737a9b642971a40652a12e831cb8217c0901091
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-12-02 20:08:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,81 @@
 function crack() {
-    paste = document.getElementsByName('latinpaste')[0].value;
-    paste = paste.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().split(/\W+/);
-    console.log(paste);
-    document.getElementsByTagName('body')[0].innerHTML = "";
+    paste = document.getElementsByName('latinpaste')[0].value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    
+    word_array = paste.split(/\W+/);
+    punc_array = paste.split(' ');
 
-    function wordSearch(word, def = 0, ogw = 0) {
+    document.getElementsByTagName('body')[0].innerHTML = '';
 
-        if (ogw == 0) {
-            ogw = word;
-        }
-        
-        var url = "https://en.wiktionary.org/w/api.php?action=parse&page=" + word + "&format=json&origin=*";
-
+    function def_card(word, punc, form_of='') {
+        url = "https://en.wiktionary.org/w/api.php?action=parse&page=" + word + "&format=json&origin=*";
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url, false);
-
         xhr.setRequestHeader("Accept", "*/*");
-
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4) {
-                raw = xhr.responseText;
-                jason = JSON.parse(xhr.responseText);
-                res = "oop";
-                if (typeof jason['parse'] !== "undefined") {
-                    if (typeof jason['parse']['text'] !== "undefined") {
-                        if (typeof jason['parse']['text']['*'] !== "undefined") {
-                            res = JSON.parse(xhr.responseText)['parse']['text']['*'];
+                
+                
+                text = xhr.responseText;
+                if (typeof text['parse'] !== undefined) {
+                    if (typeof text['parse']['text'] !== undefined) {
+                        if (typeof text['parse']['text']['*'] !== undefined) {
+                            res = text['parse']['text']['*'];
+                }}}
+                
+                content = ''
+                if (res == undefined) {
+                    if (word.substr(word.length-3) == 'que') {
+                        def_card(word.substr(0,word.length-3), punc)
+                }} else {
+                    if (res.includes('<span class="mw-headline" id="Latin">Latin</span>')) {
+                        
+                        res = res.split('<span class="mw-headline" id="Latin">Latin</span>')[1].split('<hr>')[0]
+                    
+                        if (raw.includes('Latin_non-lemma_forms') && form_of == '') {
+                            next_word = res.split('<span class="form-of-definition-link">')[1].split('<a href="/wiki/')[1].split('#Latin" title')[0]
+                            form_trace = res.split('<span class=\"form-of-definition use-with-mention\">')[1].split('</span></li>')[0]
+                            wordSearch(next_word, punc, form_trace);
+                        } else {
+                            regex = /(noun|pronoun|verb|adjective|adverb|preposition|conjunction|interjection)<\/span><span class="mw-editsection">/i, 
+                                result, indices = [];
+                            while ( (result = regex.exec(res)) ) {
+                                indices.push(result.index);
+                            }
+                            for (i = 0; i < indices.length; i++) {
+                                content += '<span>'+ res.substr(indices[i],res.search('</ol>'))
+                            }
                         }
                     }
                 }
-                if (raw.includes('Latin_non-lemma_forms') && def == 0) {
-                    res = res.substring(res.indexOf('form-of'), res.length).split('class="Latn mention" lang="la"')[1].split('href')[1].split('"', 3)[1];
-                    res = res.split('/')[2].split('#Latin')[0];
-                    //console.log('next word is ' + res);
-                    wordSearch(res, 1, word);
-                } else if (res.includes('<h2><span class="mw-headline" id="Latin">')) {
-                    res = res.split('<h2><span class="mw-headline" id="Latin">', 2)[1];
-                    res = res.substring(res.split(/(noun<\/span><span class="mw-editsection">|pronoun<\/span><span class="mw-editsection">|verb<\/span><span class="mw-editsection">|adjective<\/span><span class="mw-editsection">|adverb<\/span><span class="mw-editsection">|preposition<\/span><span class="mw-editsection">|conjunction<\/span><span class="mw-editsection">|interjection<\/span><span class="mw-editsection">)/i)[0].lastIndexOf("<h"), res.length);
-                    res = res.substring(0, res.search('</ol>'));
-                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:12%; padding-right:3%; font-size:12px; float:left;"><h2>' + ogw + '</h2>'+res+'</div>';
-                } else {
-                    document.getElementsByTagName('body')[0].innerHTML += '<div style="width:12%; padding-right:3%; font-size:12px; float:left;"><h2>' + ogw + '</h2>undefined</div>';
-                }
-                document.querySelectorAll("dl").forEach(e => e.remove());
-                document.querySelectorAll("ul").forEach(e => e.remove());
-                document.querySelectorAll(".thumbinner").forEach(e => e.remove());
-                var anchors = document.querySelectorAll("A");
-                for (var i = 0; i < anchors.length; i++) {
-                    var span = document.createElement("SPAN");
-                    if (anchors[i].className) {
-                        span.className = anchors[i].className;
-                    }
+                document.getElementsByTagName('body')[0].innerHTML += '<div style="width:12%; padding-right:3%; font-size:12px; float:left;"><h2>' + 
+                        punc + '</h2>' + content + '</div>';
 
-                    if (anchors[i].id) {
-                        span.id = anchors[i].id;
-                    }
-
-                    span.innerHTML = anchors[i].innerHTML;
-
-                    anchors[i].parentNode.replaceChild(span, anchors[i]);
-                }
             }
         }
-
         xhr.send();
     }
+    
+    for (var i = 0; i < word_array.length; i++) {
+        def_word(word_array[i], punc_array[i]);
+    }
+    
+    document.querySelectorAll("dl").forEach(e => e.remove());
+    document.querySelectorAll("ul").forEach(e => e.remove());
+    document.querySelectorAll(".thumbinner").forEach(e => e.remove());
+    var anchors = document.querySelectorAll("A");
+    for (var i = 0; i < anchors.length; i++) {
+        var span = document.createElement("SPAN");
+        if (anchors[i].className) {
+            span.className = anchors[i].className;
+        }
 
-    for (var i = 0; i < paste.length; i++) {
-        wordSearch(paste[i]);
+        if (anchors[i].id) {
+            span.id = anchors[i].id;
+        }
+
+        span.innerHTML = anchors[i].innerHTML;
+
+        anchors[i].parentNode.replaceChild(span, anchors[i]);
     }
 
 }
