0607ca1fc1a919ffe0c44ed47e072435fe89ade1
ಠ_ಠ <33814948+calculating@users.noreply.github.com>
2021-12-03 19:24:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -55,13 +55,12 @@ function crack() {
                         carded = true;
                     } else {
                         regex = /(Noun|Pronoun|Verb|Adjective|Adverb|Preposition|Particle|Participle|Determiner|Conjunction|Interjection)<\/span><span class="mw-editsection">/g;
-                        indices = res.matchAll(regex)
+                        indix = res.match(regex)
                         
                         content = ''
-                        for (const match of indices){
-                            block = res.slice(match.index, res.indexOf('</ol>',match.index))
-                            content += block.slice(0,block.search('\\[')) + block.slice(block.search('\\]')+1,block.length)
-                        }
+                        block = res.slice(indix.index, res.indexOf('</ol>',indix.index))
+                        content += block.slice(0,block.search('\\[')) + block.slice(block.search('\\]')+1,block.length)
+                        
                         content = content.replace(/<[^>]*>/g, '');
                         document.getElementById('stuff').innerHTML += '<div style="width:12%; padding-right:3%; font-size:12px; float:left;"><h2>' + punc + '</h2>' + form_of + content + '</div>';
                         carded = true;
